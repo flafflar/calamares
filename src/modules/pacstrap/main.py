d41ae4db68a7f5985b3c495bde8572a1a23715a4
flafflar <achmichail@gmail.com>
2022-01-04 13:23:09 +0200

    [pacstrap] Command is run with the Calamares utility method

--- src/modules/pacstrap/main.py
+++ src/modules/pacstrap/main.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python3
 import libcalamares
-import subprocess
 import re
+
+total_packages = -1
+downloaded_packages = 0
+installed_packages = 0
+
+def output_handler(line):
+    global total_packages, downloaded_packages, installed_packages
+    if line.startswith('Packages ('):
+        total_packages = int(re.findall('\((\d+)\)', line)[0])
+    if 'downloading' in line and total_packages != -1:
+        downloaded_packages += 1
+    if 'installing' in line and total_packages != -1:
+        installed_packages += 1
+
+    if total_packages != -1:
+        libcalamares.job.setprogress((downloaded_packages/2 + installed_packages/2) / total_packages)
 
 def run():
     """
@@ -10,19 +25,7 @@ def run():
 
     root_mount_point = libcalamares.globalstorage.value("rootMountPoint")
 
-    total_packages = -1
-    downloaded_packages = 0
-    installed_packages = 0
-    
-    process = subprocess.Popen(['sudo', 'pacstrap', root_mount_point, 'base', 'linux'], stdout=subprocess.PIPE)
-    for line in iter(process.stdout.readline, b''):
-        
-        if line.startswith(b'Packages ('):
-            total_packages = int(re.findall('\((\d+)\)', str(line))[0])
-        if b'downloading' in line and total_packages != -1:
-            downloaded_packages += 1
-        if b'installing' in line and total_packages != -1:
-            installed_packages += 1
-
-        if total_packages != -1:
-            libcalamares.job.setprogress((downloaded_packages/2 + installed_packages/2) / total_packages)
+    try:
+        libcalamares.utils.host_env_process_output(['pacstrap', root_mount_point, 'base', 'linux'], output_handler)
+    except:
+        pass
