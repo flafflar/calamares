221ad58f4ee06137a21d9be8b582078885bb1d68
flafflar <achmichail@gmail.com>
2022-01-04 11:23:08 +0200

    [pacstrap] Added a basic progress tracker to the job

--- src/modules/pacstrap/main.py
+++ src/modules/pacstrap/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import libcalamares
 import subprocess
+import re
 
 def run():
     """
@@ -9,4 +10,19 @@ def run():
 
     root_mount_point = libcalamares.globalstorage.value("rootMountPoint")
 
-    subprocess.run(['sudo', 'pacstrap', root_mount_point, 'base', 'linux'])
+    total_packages = -1
+    downloaded_packages = 0
+    installed_packages = 0
+    
+    process = subprocess.Popen(['sudo', 'pacstrap', root_mount_point, 'base', 'linux'], stdout=subprocess.PIPE)
+    for line in iter(process.stdout.readline, b''):
+        
+        if line.startswith(b'Packages ('):
+            total_packages = int(re.findall('\((\d+)\)', str(line))[0])
+        if b'downloading' in line and total_packages != -1:
+            downloaded_packages += 1
+        if b'installing' in line and total_packages != -1:
+            installed_packages += 1
+
+        if total_packages != -1:
+            libcalamares.job.setprogress((downloaded_packages/2 + installed_packages/2) / total_packages)
